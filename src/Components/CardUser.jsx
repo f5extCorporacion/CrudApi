21264a899226e9d5d9f158a775098fdffabe44f4
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2024-01-15 16:53:51 -0600

    Update CardUser.jsx
    Actualizacion del codigo todo funcional

--- src/Components/CardUser.jsx
+++ src/Components/CardUser.jsx
@@ -3,21 +3,17 @@
 const CardUser = ({users, deletePropl, updatePropl,modalopen}) => {
   
   return (
-    <div className="card  ">
-         <div className="imagen p-2">
-           <img className="imgCard rounded-lg w-[50%] h-[50%]" src={users?.image_url} all=""  />
-         </div>
+    <div className="card ">
          <div className="cardbody flex flex-col p-3 "> 
             <h2 className="flex flex-col">
-              {/*<img src={users?.image_url} alt="" className=" rounded-2xl w-7 h-7" />*/}
+            <img src={users?.image_url} alt="" className=" rounded-2xl w-7 h-7 w-56" />
               {users?.first_name} {users?.last_name} <hr /></h2>
               <span> <p>Email</p>{users?.email} </span>
               <span> <p> Birthday </p> {users?.birthday} <hr /> </span>
              
             <div className=" flex gap-1 flex-row-reverse">
-            <button className="p-2 bg-red-400 rounded-xl  border border-slate-500" onClick={()=>{deletePropl(users.id)}
-                  } > <ion-icon name="trash-outline"></ion-icon> </button>  
-
+            <button className="p-2 bg-red-400 rounded-xl  border border-slate-500" onClick={()=>{deletePropl(users.id)}}>
+              <ion-icon name="trash-outline"></ion-icon> </button>  
                <button className="p-2  rounded-xl border border-slate-500" onClick={()=>{
                 updatePropl(users);
                 modalopen(true);
